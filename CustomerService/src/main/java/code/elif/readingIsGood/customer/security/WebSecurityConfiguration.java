c5f7d7a6d7dc3be61a1eaa8b32e4a43ac3fcdfc6
elif.akgun <potrak@gmail.com>
2022-05-19 23:32:56 +0300

    added swagger3 feature

--- CustomerService/src/main/java/code/elif/readingIsGood/customer/security/WebSecurityConfiguration.java
+++ CustomerService/src/main/java/code/elif/readingIsGood/customer/security/WebSecurityConfiguration.java
@@ -20,7 +20,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public WebSecurity(Environment environment, UserService userService,
+    public WebSecurity(Environment environment,
+                       UserService userService,
                        BCryptPasswordEncoder bCryptPasswordEncoder)
     {
         this.environment = environment;
@@ -30,9 +31,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**")
+        http.authorizeRequests()
+                .antMatchers("/customer/**")
                 .hasIpAddress(environment.getProperty("apigateway.ip"))
         .and().addFilter(getAuthenticationFilter());
+
+    }
+
+    @Override
+    public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**");
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception
