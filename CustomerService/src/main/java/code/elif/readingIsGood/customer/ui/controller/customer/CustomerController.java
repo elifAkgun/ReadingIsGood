27e5aad1f4855f36cf4ae8e127f6d806ca43f917
elif.akgun <potrak@gmail.com>
2022-05-21 00:02:03 +0300

    removed user control, replaced customer order and create  methods

--- CustomerService/src/main/java/code/elif/readingIsGood/customer/ui/controller/customer/CustomerController.java
+++ CustomerService/src/main/java/code/elif/readingIsGood/customer/ui/controller/customer/CustomerController.java
@@ -1,27 +1,80 @@
 package code.elif.readingIsGood.customer.ui.controller.customer;
 
 import code.elif.readingIsGood.customer.service.CustomerService;
+import code.elif.readingIsGood.customer.service.OrderService;
+import code.elif.readingIsGood.customer.service.dto.CustomerDTO;
+import code.elif.readingIsGood.customer.service.dto.OrderDTO;
+import code.elif.readingIsGood.customer.ui.error.ApiError;
+import code.elif.readingIsGood.customer.ui.model.Customer;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
     final
     CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
+    final
+    OrderService orderService;
+
+    public CustomerController(CustomerService customerService, OrderService orderService) {
         this.customerService = customerService;
+        this.orderService = orderService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/customers/info")
-    public String authenticateCustomer(
-            @RequestHeader(name = AUTHORIZATION, required = true)String jwt,
-            @RequestParam(name = "email", required = true) String email,
-            @RequestParam(name = "password", required = true) String password) {
-
-        return  jwt;
+    @GetMapping("{customerId}/order")
+    public Page<OrderDTO> getCustomerOrder(
+            @PathVariable(name = "customerId") Integer customerId, Pageable page) {
+        return orderService.findOrdersByCustomerId(customerId,page);
     }
 
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer) {
+        ModelMapper modelMapper = new ModelMapper();
+        //Strictly matches source and destination properties
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        modelMapper.map(customer, customerDTO);
+
+        Customer customerResp = new Customer();
+        customerDTO = customerService.createCustomer(customerDTO);
+        modelMapper.map(customerDTO, customerResp);
+        customerResp.setPassword(customer.getPassword());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerResp);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ApiError handleValidationException(MethodArgumentNotValidException exception,
+                                       HttpServletRequest request) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        Map<String, String> validationErrors = new HashMap<String, String>();
+
+        for (FieldError fieldError: bindingResult.getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ApiError apiError = new ApiError(400, "ValidationError", request.getServletPath());
+        apiError.setValidationErrors(validationErrors);
+
+        return apiError;
+    }
 
 }
--- CustomerService/src/main/java/code/elif/readingIsGood/customer/ui/controller/order/OrderController.java
+++ CustomerService/src/main/java/code/elif/readingIsGood/customer/ui/controller/order/OrderController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.PathParam;
 import java.util.List;
 
 @RestController
@@ -26,5 +28,11 @@ public class OrderController {
     public Page<OrderDTO> getCustomerOrder(
             @RequestParam(name = "customerId") Integer customerId, Pageable page) {
         return orderService.findOrdersByCustomerId(customerId,page);
+    }
+
+    @GetMapping("/order/{id}")
+    public OrderDTO getOrderById(
+            @PathVariable("id") Integer id) {
+        return orderService.findOrderById(id);
     }
 }
